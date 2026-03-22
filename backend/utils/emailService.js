2495b56bd0b82e675910c8571b7bb8bd29846d09
Mohammed Tharick <mdtharick25@gmail.com>
2026-03-22 17:26:00 +0530

    mail fix for render

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -2,10 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // Helps bypass some cloud network handshake issues
+    minVersion: "TLSv1.2",
   },
 });
 
